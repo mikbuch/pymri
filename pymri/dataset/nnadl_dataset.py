52b2fd463e54f4344ed2b288a82645fa5a02579e
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2015-11-03 23:47:37 +0100

    nnadl_dataset (including load_nifti) working

--- pymri/dataset/nnadl_dataset.py
+++ pymri/dataset/nnadl_dataset.py
@@ -8,7 +8,41 @@ __contact__ = "mikolaj.buchwald@gmail.com"
 
 import numpy as np
 
-def load_nifti(data_dir):
+
+def load_nifti(data_dir, Y):
+    '''
+    Parameters
+    ----------
+    data_dir : string.
+        Location of the data files (bold.nii.gz, attributes.txt,
+        attributes_literal.txt).
+
+    Y : ndtuple of strings.
+        Classes. Label space will be reduced to specified conditions.
+
+
+    Returns
+    -------
+    X : ndnumpy array.
+        Samples containing features.
+
+    y : ndnumpy array.
+        Labels, targets, classes.
+
+
+    conditions:
+    Y = {Y_1, Y_2, ... , Y_n}
+    Y_n = S = {S_1, S_2, .... , S_k}
+
+    where:
+        Y - set of classes
+        S - set of subclasses
+        n - number of classes
+        k - number of subclasses
+
+    classes consists of subclasses
+
+    '''
     import numpy as np
     import nibabel
     from sklearn.datasets.base import Bunch
@@ -31,31 +65,32 @@ def load_nifti(data_dir):
     conditions = np.recfromtxt(dataset_files.conditions_target)['f0']
     mask = dataset_files.mask
 
-
-
     # ### Restrict to specified conditions
 
+    condition_mask = np.zeros(shape=(conditions.shape[0]))
 
-    # condition_mask = np.logical_or(conditions == 'face', conditions == 'house')
-    # condition_mask = np.logical_or(
-        # np.logical_or(
-            # conditions == 'PlanCtrl_5', conditions == 'PlanCtrl_0'
-            # ),
-        # conditions == 'Rest'
-        # )
-    condition_mask = np.logical_or(
-        np.logical_or(
-            conditions == 'PlanCtrl_5', conditions == 'PlanCtrl_0'
-            ),
-        np.logical_or(
-            conditions == 'ExeCtrl_5', conditions == 'ExeCtrl_0'
-            )
-        )
+    for n in Y:
+        if type(n) == tuple:
+            # first label
+            k_uniform = n[0]
+            k_uniform = y[conditions == k_uniform][0]
+            for k in n:
+                condition_mask += conditions == k
+                # unifying subclasses into one class
+                # (string label doesn't matter)
+                y[conditions == k] = k_uniform
+        else:
+            condition_mask += conditions == n
+
+    condition_mask = np.array(condition_mask, dtype=bool)
     X = fmri_data[..., condition_mask]
     y = y[condition_mask]
 
-    from sklearn.preprocessing import binarize
-    y = binarize(y, threshold=3.0)[0]
+    cnt = 0
+    for val in np.unique(y):
+        if val > cnt:
+            y[y == val] = cnt
+        cnt += 1
 
     # ### Masking step
     from pymri.utils import masking
@@ -75,96 +110,27 @@ def load_nifti(data_dir):
     feature_selection.fit(X, y)
     X = feature_selection.transform(X)
 
+    # normalize data
     from sklearn import preprocessing
     X = preprocessing.normalize(X)
 
     X = np.reshape(X, (X.shape[0], X.shape[1], 1))
-    n_classes = 2
+
+    # how many classes do we have?
+    n_classes = len(Y)
     y = convertToOneOfMany(y, n_classes)
     y = np.reshape(y, (y.shape[0], y.shape[1], 1))
 
     return X, y
 
-def load_nnadl_dataset(data_dir, sizes=(0.5, 0.25, 0.25)):
+def load_nnadl_dataset(data_dir, Y, sizes=(0.5, 0.25, 0.25)):
     """
-    00. Load data from file (X and y).
-    01. Split into training phase (train dataset) and validation phase.
-    02. Split validation phase into valdation dataset and test dataset.
 
     Datasets default proportions:
     (train/validation/test) (0.5/0.25/0.25)
     """
 
-    import numpy as np
-    import nibabel
-    from sklearn.datasets.base import Bunch
-
-    data_dir = data_dir
-
-    # create sklearn's Bunch of data
-    dataset_files = Bunch(
-        func=data_dir+'bold.nii.gz',
-        session_target=data_dir+'attributes.txt',
-        mask=data_dir+'mask.nii.gz',
-        conditions_target=data_dir+'attributes_literal.txt'
-        )
-
-    # fmri_data and mask are copied to break reference to the original object
-    bold_img = nibabel.load(dataset_files.func)
-    fmri_data = bold_img.get_data().astype(float)
-    affine = bold_img.get_affine()
-    y, session = np.loadtxt(dataset_files.session_target).astype("int").T
-    conditions = np.recfromtxt(dataset_files.conditions_target)['f0']
-    mask = dataset_files.mask
-
-    # ### Restrict to specified conditions
-
-    # condition_mask = np.logical_or(conditions == 'face', conditions == 'house')
-    # condition_mask = np.logical_or(
-        # np.logical_or(
-            # conditions == 'PlanCtrl_5', conditions == 'PlanCtrl_0'
-            # ),
-        # conditions == 'Rest'
-        # )
-    condition_mask = np.logical_or(
-        np.logical_or(
-            conditions == 'PlanCtrl_5', conditions == 'PlanCtrl_0'
-            ),
-        np.logical_or(
-            conditions == 'ExeCtrl_5', conditions == 'ExeCtrl_0'
-            )
-        )
-    X = fmri_data[..., condition_mask]
-    y = y[condition_mask]
-
-    from sklearn.preprocessing import binarize
-    y = binarize(y, threshold=3.0)[0]
-
-    # ### Masking step
-    from pymri.utils import masking
-    from nibabel import Nifti1Image
-
-    # Mask data
-    X_img = Nifti1Image(X, affine)
-    X = masking.apply_mask(X_img, mask, smoothing_fwhm=4)
-
-    from sklearn.feature_selection import SelectKBest, f_classif
-
-    # ### Define the dimension reduction to be used.
-    # Here we use a classical univariate feature selection based on F-test,
-    # namely Anova. We set the number of features to be selected to 784
-    feature_selection = SelectKBest(f_classif, k=784)
-
-    feature_selection.fit(X, y)
-    X = feature_selection.transform(X)
-
-    from sklearn import preprocessing
-    X = preprocessing.normalize(X)
-
-    X = np.reshape(X, (X.shape[0], X.shape[1], 1))
-    n_classes = 2
-    y = convertToOneOfMany(y, n_classes)
-    y = np.reshape(y, (y.shape[0], y.shape[1], 1))
+    X, y = load_nifti(data_dir, Y)
 
     # ### Splitting ###########################################################
     from sklearn.cross_validation import train_test_split
