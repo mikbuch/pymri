d45aab3fca0affe00c0062e1bd1d3553c116804e
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2015-11-08 22:59:41 +0100

    examples/ga/inputs.py working, modified load data and range of values in individual vector

--- examples/ga/inputs.py
+++ examples/ga/inputs.py
@@ -6,17 +6,73 @@ from deap import base
 from deap import creator
 from deap import tools
 
-from pymri.dataset.nnadl_dataset import load_nifti
-from pymri.genetic_algorithm.fitness_functions import test_network
 
+# #### NETWORK AND GENETIC ALGORITH VARIABLES
+
+# number of features (to be extracted, selected) it is equal to number of
+# inputs of the ann as well as number of elements of the vector being
+# the individual, member of the population for genetic algorithm
+k_features = 50
+hidden_neurons = 25
+
+# #### DatastManager initialization
+
+# TODO: QUESTION: can I scale the values in range 0:1?
+
+from pymri.dataset import DatasetManager
+# dataset settings
+ds = DatasetManager(
+    path_input='/home/jesmasta/amu/master/nifti/bold/',
+    contrast=(('ExeTool_0', 'ExeTool_5'), ('ExeCtrl_0', 'ExeCtrl_5')),
+    k_features = k_features,
+    normalize = True,
+    # scale_0_1 = True,
+    nnadl = True,
+    sizes=(0.75, 0.25)
+    )
+# load data
+ds.load_data()
+
+# select feature reduction method
+ds.feature_reduction(roi_selection='SelectKBest')
+
+# get training, validation and test datasets for specified roi
+training_data, validation_data, test_data = ds.split_data()
+
+
+def random_min_max():
+    return random.uniform(ds.training_data_min, ds.training_data_max)
+
+
+# set up network
+from pymri.model import fnn
+decision = 'y'
+while decision == 'y':
+    net = fnn.Network([k_features, hidden_neurons, 2])
+    net.SGD(training_data, 100, 11, 2.961, test_data=test_data)
+    print('\nBest score: %d' % (net.best_score))
+    print('Continue training? (y/n + enter)'),
+    decision = raw_input()
+
+net.biases = net.best_biases
+net.weights = net.best_weights
+
+
+##############################################################################
+#
+#   GENETIC ALGORITHM SETUP
+#
+##############################################################################
 
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", np.ndarray, fitness=creator.FitnessMax)
 
-IND_SIZE=50
+IND_SIZE = k_features
+
 
 toolbox = base.Toolbox()
-toolbox.register("attr_float", random.random)
+# toolbox.register("attr_float", random.random)
+toolbox.register("attr_float", random_min_max)
 toolbox.register("individual", tools.initRepeat, creator.Individual,
                  toolbox.attr_float, n=IND_SIZE)
 
@@ -24,38 +80,15 @@ toolbox.register("individual", tools.initRepeat, creator.Individual,
 # define the population to be a list of 'individual's
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-# TODO: adjust that or maybe no need with float vector
-# def decode_bin(bin_list):
-    # return int(''.join(map(str, bin_list)), 2)
- 
-
-from pymri.dataset import load_nnadl_dataset
-path = '/home/jesmasta/amu/master/nifti/bold/'
-training_data, validation_data, test_data = load_nnadl_dataset(
-    path,
-    (('ExeTool_0', 'ExeTool_5'), ('ExeCtrl_0', 'ExeCtrl_5')),
-    k_features = 50,
-    normalize = True,
-    scale_0_1 = True,
-    sizes=(0.75, 0.25)
-    )
-
-from pymri.model import fnn
-decision = 'y'
-while decision == 'y':
-    net = fnn.Network([50, 25, 2])
-    net.SGD(training_data, 100, 11, 2.961, test_data=test_data)
-    print('\nBest score: %d' % (net.best_score))
-    print('Continue training? (y/n + enter)'),
-    decision = raw_input()
-    
-net.biases = net.best_biases
-net.weights = net.best_weights
-
-# TODO: number of features (reduced with PCA or selectkbest)
-
+##############################################################################
+#
+#   FITNESS FUCNTION
+#
+##############################################################################
 from pymri.genetic_algorithm import get_prob_class
 # the goal ('fitness') function to be maximized
+
+
 def evalOneMax(individual):
     return get_prob_class(
         net, np.reshape(individual, (individual.shape[0], 1)), 0
@@ -83,7 +116,6 @@ toolbox.register("select", tools.selRoulette)
 def main():
     random.seed(64)
 
-
     # create an initial population of 300 individuals (where
     # each individual is a list of integers)
     pop = toolbox.population(n=800)
@@ -95,7 +127,7 @@ def main():
     #
     # NGEN  is the number of generations for which the
     #       evolution runs
-    CXPB, MUTPB, NGEN = 0.5, 0.8, 1000
+    CXPB, MUTPB, NGEN = 0.5, 0.8, 10
 
     mean_log = np.zeros(shape=(NGEN,))
     min_log = np.zeros(shape=(NGEN,))
@@ -137,7 +169,7 @@ def main():
             # mutate an individual with probability MUTPB
             if random.random() < MUTPB:
 
-                mutant[random.randint(0,len(mutant)-1)] = random.random()
+                mutant[random.randint(0, len(mutant)-1)] = random_min_max()
                 del mutant.fitness.values
 
         # Evaluate the individuals with an invalid fitness
@@ -147,7 +179,6 @@ def main():
             ind.fitness.values = fit
 
         # print("  Evaluated %i individuals" % len(invalid_ind))
-
 
         # The population is entirely replaced by the offspring
         pop[:] = offspring
@@ -174,11 +205,22 @@ def main():
     best_ind = tools.selBest(pop, 1)[0]
     print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
 
+    # visualize evolution
     import matplotlib.pyplot as plt
     plt.plot(min_log)
     plt.plot(max_log)
     # plt.errorbar(np.arange(len(mean_log)), mean_log, yerr=std_log*3)
     plt.show()
+
+    ############################################
+    #
+    #   SAVE ACTIVATION PATTERN AS NIFTI
+    #
+    ############################################
+    activation = np.array(best_ind)
+
+    # save activation to nifti file
+    ds.save_as_nifti(activation, 'prototype_activation')
 
 
 if __name__ == "__main__":
