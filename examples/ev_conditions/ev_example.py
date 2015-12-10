e4cd1a9a729b478a49fc12829e9bf573fd308418
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2015-12-10 15:34:10 +0100

    examples/ev_example.py create mvpa dir if it doesn't exist

--- examples/ev_conditions/ev_example.py
+++ examples/ev_conditions/ev_example.py
@@ -6,11 +6,15 @@ Use this script to generate all 'attributes.txt' and 'attributes_literal.txt'
 files. They contain information about conditions in particular volumens.
 '''
 
+import os
+
 from pymri.ev_conditions import get_attributes
 
 base_directory = '/tmp/Maestro_Project1/'
+# base_directory = '/Users/AClab/Documents/mikbuch/Maestro_Project1/'
 subject_template = 'GK'
 ev_template = base_directory + '%s/EVs/S_%s_%s/'
+hand_template = base_directory + '%s/%s_Hand/'
 mvpa_template = base_directory + '%s/%s_Hand/mvpa/'
 
 from pymri.utils.paths_dirs_info import get_subject_names
@@ -20,8 +24,17 @@ hands = ['Left', 'Right']
 
 for sub in subjects:
     for hand in hands:
+        ev_dir = ev_template % (sub, sub[2:5], hand[0])
+        hand_dir = hand_template % (sub, hand)
+        mvpa_dir = mvpa_template % (sub, hand)
+        # check if proper subject_hand directory exists (is properly pointed)
+        if os.path.exists(hand_dir):
+            # create mvpa directory (for sub_hand) if it doesn't exist
+            if not os.path.exists(mvpa_dir):
+                os.makedirs(mvpa_dir)
         get_attributes(
-            input_dir=ev_template % (sub, sub[2:5], hand[0]),
+            input_dir=ev_dir,
             tr=2,
-            output_dir=mvpa_template % (sub, hand)
+            output_dir=mvpa_dir,
+            quiet=False
             )
