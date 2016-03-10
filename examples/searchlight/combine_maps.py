ce2a9ae99b9ea0f2e760034d5344b4a86fbc5791
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2016-03-10 15:53:45 +0100

    combine maps visalization reorganization and minor changes

--- examples/searchlight/combine_maps.py
+++ examples/searchlight/combine_maps.py
@@ -23,13 +23,17 @@ import matplotlib.pyplot as plt
 from scipy import stats
 from statsmodels.sandbox.stats.multicomp import multipletests
 
+
 ############################################
 #       PATHS AND SETTINGS
 ############################################
 
 # Preloaded maps in numpy array format or separate nifti files
-numpy_array_load = True
 maps_path = '/amu/master/neuronus_2016_searchlight/results/maps.npy'
+if maps_path.endswith('.npy'):
+    numpy_array_load = True
+else:
+    numpy_array_load = False
 mask_path = os.path.join(
     os.environ['FSLDIR'], 'data/standard/MNI152_T1_2mm_brain_mask.nii.gz'
     )
@@ -56,6 +60,7 @@ bg_brain_path = os.path.join(
 mask_img = nib.load(mask_path)
 mask = mask_img.get_data().astype(bool)
 
+
 ############################################
 #   LOAD INFORMATION BASED MAPS
 ############################################
@@ -69,19 +74,17 @@ else:
 
     for file in os.listdir(maps_path):
         if fnmatch(file, '*MNI152.nii.gz'):
-            files.append(file)
+            files.append(os.path.join(maps_path, file))
 
     maps = np.zeros((len(files), mask.sum()))
 
     for i in range(len(files)):
         maps[i] = nib.load(files[i]).get_data()[mask]
 
+
 ############################################
 #   COMBINE MAPS
 ############################################
-
-# Load maps registered to mni152
-maps = np.load(maps_path)
 
 # Averege maps
 # mean_map = maps.mean(axis=0)
@@ -91,26 +94,6 @@ mean_map_unmasked = np.zeros(mask.shape)
 mean_map_unmasked[mask] = mean_map
 mean_map_img = nib.Nifti1Image(mean_map_unmasked, mask_img.get_affine())
 nib.save(mean_map_img, os.path.join(output_directory, mean_map_filename))
-
-# plot classification distributions
-fig, ax = plt.subplots(figsize=(20, 10))
-n, bins, patches = plt.hist(mean_map[mean_map > 0], bins=31)
-cm = plt.cm.get_cmap('autumn')
-bin_centers = 0.5 * (bins[:-1] + bins[1:])
-col = bin_centers - min(bin_centers)
-col /= max(col)
-for c, p, v in zip(col, patches, bins):
-    if v > 0.5:
-        plt.setp(p, 'facecolor', cm(c-0.5*(1-c)))
-    else:
-        plt.setp(p, 'facecolor', 'white')
-
-ax.tick_params(axis='x', pad=20)
-ax.set_xlabel('accuracy score', labelpad=15)
-ax.set_ylabel('classifications performed', labelpad=30)
-
-plt.rcParams.update({'font.size': 28})
-plt.tight_layout()
 
 # Create maps mask. Take only voxels which has informations from all maps
 maps_mask = (maps.astype('bool').sum(axis=0) == maps.shape[0])
@@ -138,9 +121,36 @@ zscore[maps_mask] = zscore_masked
 thr_zscore = np.zeros(mask.shape)
 thr_zscore[mask] = zscore
 
+############################################
+#   SAVE RESULTS
+############################################
 thr_zscore_img = nib.Nifti1Image(thr_zscore, mask_img.get_affine())
 nib.save(thr_zscore_img, os.path.join(output_directory, zscore_map_filename))
 
+
+############################################
+#   VISUALISE RESULTS
+############################################
+
+# plot classification distributions
+fig, ax = plt.subplots(figsize=(20, 10))
+n, bins, patches = plt.hist(mean_map[mean_map > 0], bins=31)
+cm = plt.cm.get_cmap('autumn')
+bin_centers = 0.5 * (bins[:-1] + bins[1:])
+col = bin_centers - min(bin_centers)
+col /= max(col)
+for c, p, v in zip(col, patches, bins):
+    if v > 0.5:
+        plt.setp(p, 'facecolor', cm(c-0.5*(1-c)))
+    else:
+        plt.setp(p, 'facecolor', 'white')
+ax.tick_params(axis='x', pad=20)
+ax.set_xlabel('accuracy score', labelpad=15)
+ax.set_ylabel('classifications performed', labelpad=30)
+plt.rcParams.update({'font.size': 28})
+plt.tight_layout()
+
+# open in fslview
 import subprocess as sp
 cmd = 'fslview %s -l Grey %s -l Red-Yellow %s -l Blue-Lightblue -b %f,%f' % (
     bg_brain_path,
