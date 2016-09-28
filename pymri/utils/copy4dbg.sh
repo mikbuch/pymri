e933a9d211e0d60654bdbb2f8390f78eeb0fc46d
Heraklit <AClab@Heraklits-iMac.local>
2016-09-28 13:20:55 +0200

    copy4dbg utility It helps user to copy the data to perform debugging witouth thrashing source (base) directory.

---
+++ pymri/utils/copy4dbg.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+ARGS="$@"
+set -- $ARGS
+
+base_dir=$1
+output_dir=$2/$(basename $base_dir)
+
+subjects=$(ls -1 $1 | grep 'GK')
+subjects='GK011RZJA GK012OHPA'
+
+echo
+echo
+echo 'base_dir: '$base_dir
+echo 'output_dir: '$output_dir
+echo
+echo 'subjects:'
+echo $subjects
+echo
+
+echo $'Creating output directory (if doesn\'t already exists) at:'
+echo $output_dir
+mkdir -p $output_dir
+echo
+
+echo
+echo 'Copying from:'
+echo $base_dir
+echo 'Copying data started ...'
+echo
+for sub in $subjects
+do
+    echo
+    echo 'SUBJECT: '$sub
+    echo
+    echo 'Create directory'
+    mkdir -p $output_dir/$sub
+    echo 'Copying data for this subject begins ...'
+    cp -r $base_dir/$sub/localizer $output_dir/$sub
+    echo '... copying data for this subject finished.'
+    echo
+done
+
+echo
+echo '... copying all data finished.'
+echo
